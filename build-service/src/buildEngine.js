60ecb0e6d4edf00f44e140e68882f455b9186431
HACKMASTER-TR <28550040284a@gmail.com>
2026-08-23 22:39:14 +0300

    Increase Gradle metaspace for AAB builds

--- build-service/src/buildEngine.js
+++ build-service/src/buildEngine.js
@@ -1048,7 +1048,7 @@ include(":app")
 
   await fs.writeFile(
     path.join(projectDir, "gradle.properties"),
-`org.gradle.jvmargs=-Xmx320m -XX:MaxMetaspaceSize=192m -XX:+UseSerialGC -Dfile.encoding=UTF-8
+`org.gradle.jvmargs=-Xmx320m -XX:MaxMetaspaceSize=256m -XX:+UseSerialGC -Dfile.encoding=UTF-8
 org.gradle.workers.max=1
 org.gradle.parallel=false
 org.gradle.vfs.watch=false
@@ -3775,7 +3775,7 @@ async function runGradle(
               ...env,
               GRADLE_OPTS:
                 "-Xmx320m " +
-                "-XX:MaxMetaspaceSize=192m " +
+                "-XX:MaxMetaspaceSize=256m " +
                 "-XX:+UseSerialGC " +
                 "-Dfile.encoding=UTF-8"
             }
